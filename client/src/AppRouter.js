d5061e61d14f3e85d3e0ea0dd5b4eaa7a6673367
Neiro Somemiya <smmy@neiro.work>
2020-08-19 21:05:30 +0900

    node-sass導入

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./reset.css";
-import "./nk.css";
+import "./nk.scss";
 
 import Index from "./components/Index";
 import Checklists from "./components/Checklists";
