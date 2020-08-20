30ee3c32f0a65e79c722de7ada830f3e26abcd62
Neiro Somemiya <smmy@neiro.work>
2020-08-20 17:50:51 +0900

    デザイン調整

--- client/src/components/Checklists/Events/index.js
+++ client/src/components/Checklists/Events/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Index = (props) => (
   <div>
     <p>マイフェス2020 機材チェックリスト</p>
-    <div>
+    <div className="nk">
       <span className="nk_radioGroup">
         <input id="confirmRadio" type="radio" name="operation" checked />
         <label className="nk_label" for="confirmRadio">
@@ -19,41 +19,18 @@ const Index = (props) => (
       <input className="nk nk_input" type="text" placeholder="個体コード" />
     </div>
     <div>
-      <div className="nk nk_equipCardContainer">
-        <div className="nk_equipCard">
-          <div className="nk_equipCard_title">
-            ATEM Mini Pro<span className="nk_equipCard_code">TK-0001</span>
+      <div className="nk_cardContainer">
+        <div className="nk_card">
+          <div className="nk_card_title">
+            ATEM Mini Pro<span className="nk_card_code">TK-0001</span>
           </div>
-          <div className="nk_equipCard_control">
-            <button className="nk_equipCard_control_confirm">確認</button>
-            <button className="nk_equipCard_control_clear">取消</button>
-          </div>
-        </div>
-        <div className="nk_equipCard">
-          <div className="nk_equipCard_title">
-            ATEM Mini Pro<span className="nk_equipCard_code">TK-0001</span>
-          </div>
-          <div className="nk_equipCard_control">
-            <button className="nk_equipCard_control_confirm">確認</button>
-            <button className="nk_equipCard_control_clear">取消</button>
-          </div>
-        </div>
-        <div className="nk_equipCard">
-          <div className="nk_equipCard_title">
-            ATEM Mini Pro<span className="nk_equipCard_code">TK-0001</span>
-          </div>
-          <div className="nk_equipCard_control">
-            <button className="nk_equipCard_control_confirm">確認</button>
-            <button className="nk_equipCard_control_clear">取消</button>
-          </div>
-        </div>
-        <div className="nk_equipCard">
-          <div className="nk_equipCard_title">
-            ATEM Mini Pro<span className="nk_equipCard_code">TK-0001</span>
-          </div>
-          <div className="nk_equipCard_control">
-            <button className="nk_equipCard_control_confirm">確認</button>
-            <button className="nk_equipCard_control_clear">取消</button>
+          <div className="nk_card_control">
+            <button className="nk_button nk_button-inline nk_button-danger">
+              取消
+            </button>
+            <button className="nk_button nk_button-inline nk_button-success">
+              確認
+            </button>
           </div>
         </div>
       </div>
