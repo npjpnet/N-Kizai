b66fcd678350b02fb2532a66b1097b91e6277dc2
Neiro Somemiya <smmy@neiro.work>
2020-08-21 20:17:13 +0900

    ログインページ作成

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "./reset.css";
 import "./nk.scss";
 
+import pkg from "./../package.json";
+
 import Index from "./components/Index";
+import Login from "./components/Index/login";
 import Checklists from "./components/Checklists";
 import EventChecklists from "./components/Checklists/Events";
 
@@ -12,7 +15,13 @@ const AppRouter = (props) => (
   <div>
     <Router>
       <div>
+        <p>N-Kizai v{pkg.version}</p>
+        <Link className="nk nk_button" to="/">
+          メインメニュー
+        </Link>
+
         <Route exact path="/" component={Index} />
+        {/* <Route exact path="/login" component={Login} /> */}
         <Route exact path="/checklists" component={Checklists} />
         <Route
           exact
---
+++ client/src/components/Index/login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Index = (props) => (
+  <div>
+    <p>ログインしてください</p>
+    <div>
+      <form>
+        <input type="text" className="nk nk_input" placeholder="ログインID" />
+        <input
+          type="password"
+          className="nk nk_input"
+          placeholder="パスワード"
+        />
+        <button type="button" className="nk nk_button">
+          ログイン
+        </button>
+      </form>
+
+      <p>または、N-Kizaiキーをスキャンしてください。</p>
+      <input
+        type="password"
+        className="nk nk_input"
+        placeholder="N-Kizaiキー"
+      />
+    </div>
+  </div>
+);
+
+export default Index;
