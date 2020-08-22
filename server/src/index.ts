7450581c38a58f604fadf5e131aa11763587ea56
Neiro Somemiya <smmy@neiro.work>
2020-08-23 06:55:26 +0900

    クラス化

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+class Kizai {
+  private msg: string;
+
+  constructor(msg: string) {
+    this.msg = msg;
+  }
+
+  public start() {
+    console.log(this.msg);
+  }
+}
+
+const kizai = new Kizai('N-Kizai');
+kizai.start();
