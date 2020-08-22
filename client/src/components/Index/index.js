116236bba3e926fae98525e1d42e39ee6da8a4a2
Neiro Somemiya <smmy@neiro.work>
2020-08-22 22:13:54 +0900

    機材登録ボタンを有効化

--- client/src/components/Index/index.js
+++ client/src/components/Index/index.js
@@ -11,7 +11,7 @@ const Index = (props) => (
       <Link to="/" className="nk nk_button">
         予約作成
       </Link>
-      <Link to="/" className="nk nk_button">
+      <Link to="/devices/add-products" className="nk nk_button">
         機材登録
       </Link>
       <Link to="/" className="nk nk_button">
