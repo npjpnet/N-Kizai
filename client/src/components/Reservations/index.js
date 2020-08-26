90ee8012ffff9b11f9a2f1390475220c06afff6c
Neiro Somemiya <smmy@neiro.work>
2020-08-26 21:31:13 +0900

    予約モック作成

--- client/src/components/Reservations/index.js
+++ client/src/components/Reservations/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import EquipCard from './EquipCard';
 
+const searchedProducts = [
+  {
+    name: 'U2C-BF30BK',
+    maker: 'エレコム',
+    genre: 'pa',
+    devices: [
+      { code: 'NP-0014', serialNumber: '', status: 'ready', remarks: '' },
+    ],
+  },
+  {
+    name: 'T-T02-3650WH/RS',
+    maker: 'エレコム',
+    genre: 'oa',
+    devices: [
+      { code: 'NP-0044', serialNumber: '', status: 'ready', remarks: '' },
+    ],
+  },
+  {
+    name: 'T-T02-3650WH/RS',
+    maker: 'エレコム',
+    genre: 'oa',
+    devices: [
+      { code: 'NP-0048', serialNumber: '', status: 'ready', remarks: '' },
+    ],
+  },
+];
+
 const AddProduct = (props) => {
+  const [searchText, setSearchText] = useState('');
+  const [selectDevice, setSelectDevice] = useState('');
+
+  const SearchedDevices = (props) =>
+    searchedProducts.map((product) =>
+      product.devices.map((device) => {
+        return (
+          <option key={device.code} value={device.code}>
+            {device.code} : {product.name}
+          </option>
+        );
+      })
+    );
+
   return (
     <div>
       <p>機材の使用予約を行います。</p>
@@ -12,11 +53,22 @@ const AddProduct = (props) => {
             <option value="">イベントを選択してください</option>
             <option value="myfes2020">マイフェス2020</option>
           </select>
-          <input type="text" className="nk nk_input" placeholder="横断検索" />
-          <select className="nk nk_select">
+          <input
+            type="text"
+            className="nk nk_input"
+            placeholder="横断検索"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <select
+            className="nk nk_select"
+            value={selectDevice}
+            onChange={(e) => setSelectDevice(e.target.value)}
+          >
             <option value="">機材を選択してください</option>
+            <SearchedDevices />
           </select>
-          <input type="text" className="nk nk_input" placeholder="備考" />
+          <input type="text" className="nk nk_input" placeholder="特記事項" />
           <button type="button" className="nk nk_button">
             予約登録
           </button>
