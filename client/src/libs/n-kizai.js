130520ac7096ca1db4fe8e42526442980a4e220b
Neiro Somemiya <smmy@neiro.work>
2020-08-27 23:56:53 +0900

    APIクラス化

---
+++ client/src/libs/n-kizai.js
@@ -0,0 +1,9 @@
+export default class {
+  getEvents() {
+    return [
+      { name: 'マイフェス2020', slug: 'myfes2020' },
+      { name: 'マイフェス2021', slug: 'myfes2021' },
+      { name: 'マイフェス2022', slug: 'myfes2022' },
+    ];
+  }
+}
