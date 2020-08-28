997f9aed81a9439ec4e319516aad894e99b9202b
Neiro Somemiya <smmy@neiro.work>
2020-08-28 21:33:00 +0900

    historyによるページ遷移

--- client/src/components/routes/Checklists/index.js
+++ client/src/components/routes/Checklists/index.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Core from './../../../libs/n-kizai';
 const core = new Core();
 
 const Index = (props) => {
   const [eventSulg, setEventSulg] = useState('');
+
+  const history = useHistory();
+  const pushHistory = (path) => history.push(path);
+
   const EventsSelector = (props) =>
-    core
-      .getEvents()
-      .map((event) => <option value={event.slug}>{event.name}</option>);
+    core.getEvents().map((event) => (
+      <option key={event.slug} value={event.slug}>
+        {event.name}
+      </option>
+    ));
 
   return (
     <div>
@@ -22,7 +29,18 @@ const Index = (props) => {
           <option value="">イベントを選択してください</option>
           <EventsSelector />
         </select>
-        <button className="nk nk_button nk_button-success">送信</button>
+        <button
+          className="nk nk_button nk_button-success"
+          onClick={() => {
+            if (!eventSulg) {
+              alert('イベントを選択してください');
+              return;
+            }
+            pushHistory(`/checklists/${eventSulg}`);
+          }}
+        >
+          送信
+        </button>
         {/* <Link to="/checklists/myfes2020" className="nk nk_button">
         送信
         </Link> */}
--- client/src/components/routes/Reservations/index.js
+++ client/src/components/routes/Reservations/index.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Core from './../../../libs/n-kizai';
 const core = new Core();
 
 const Index = (props) => {
   const [eventSulg, setEventSulg] = useState('');
+
+  const history = useHistory();
+  const pushHistory = (path) => history.push(path);
+
   const EventsSelector = (props) =>
-    core
-      .getEvents()
-      .map((event) => <option value={event.slug}>{event.name}</option>);
+    core.getEvents().map((event) => (
+      <option key={event.slug} value={event.slug}>
+        {event.name}
+      </option>
+    ));
 
   return (
     <div>
@@ -23,13 +29,24 @@ const Index = (props) => {
           <option value="">イベントを選択してください</option>
           <EventsSelector />
         </select>
-        {/* <button className="nk nk_button">送信</button> */}
-        <Link
+        <button
+          className="nk nk_button"
+          onClick={() => {
+            if (!eventSulg) {
+              alert('イベントを選択してください');
+              return;
+            }
+            pushHistory(`/reservations/${eventSulg}`);
+          }}
+        >
+          送信
+        </button>
+        {/* <Link
           to="/reservations/myfes2020"
           className="nk nk_button nk_button-success"
         >
           送信
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
