51254247bfd8cfbe065e7d46f0b76f23318b0f2e
Neiro Somemiya <smmy@neiro.work>
2020-08-30 15:58:18 +0900

    簡潔化

--- client/src/libs/n-kizai.js
+++ client/src/libs/n-kizai.js
@@ -1,4 +1,12 @@
 export default class {
+  addProducts(product) {
+    return { id: '0123456789ABCDEF' };
+  }
+
+  addDeviceByProductId(productId, device) {
+    return { code: 'NW-0334' };
+  }
+
   getEvents() {
     return [
       { name: 'マイフェス2020', slug: 'myfes2020' },
